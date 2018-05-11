f742ab47f0b832651a8852531252c7a67b2f5b51
ianbrucey <ib708090@gmail.com>
2018-05-10 21:18:12 -0700

    changing the way the urls are rendered on the landing page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,24 +9,24 @@
     <title>Otruvez</title>
 
     <!-- PLUGINS CSS STYLE -->
-    {{--<link href="{{asset('classimax/plugins/jquery-ui/jquery-ui.min.css')}}" rel="stylesheet">--}}
+    {{--<link href="{{baseUrlConcat('/classimax/plugins/jquery-ui/jquery-ui.min.css')}}" rel="stylesheet">--}}
     <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
     <!-- Bootstrap -->
-    <link href="{{asset('classimax/plugins/bootstrap/dist/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{baseUrlConcat('/classimax/plugins/bootstrap/dist/css/bootstrap.min.css')}}" rel="stylesheet">
     <!-- Font Awesome -->
-    <link href="{{asset('classimax/plugins/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet">
+    <link href="{{baseUrlConcat('/classimax/plugins/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet">
     <!-- Owl Carousel -->
-    <link href="{{asset('classimax/plugins/slick-carousel/slick/slick.css')}}" rel="stylesheet">
-    <link href="{{asset('classimax/plugins/slick-carousel/slick/slick-theme.css')}}" rel="stylesheet">
+    <link href="{{baseUrlConcat('/classimax/plugins/slick-carousel/slick/slick.css')}}" rel="stylesheet">
+    <link href="{{baseUrlConcat('/classimax/plugins/slick-carousel/slick/slick-theme.css')}}" rel="stylesheet">
     <!-- Fancy Box -->
-{{--    <link href="{{asset('classimax/plugins/fancybox/jquery.fancybox.pack.css')}}" rel="stylesheet">--}}
-    {{--<link href="{{asset('classimax/plugins/jquery-nice-select/css/nice-select.css')}}" rel="stylesheet">--}}
-    {{--<link href="{{asset('classimax/plugins/seiyria-bootstrap-slider/dist/css/bootstrap-slider.min.css')}}" rel="stylesheet">--}}
+{{--    <link href="{{baseUrlConcat('/classimax/plugins/fancybox/jquery.fancybox.pack.css')}}" rel="stylesheet">--}}
+    {{--<link href="{{baseUrlConcat('/classimax/plugins/jquery-nice-select/css/nice-select.css')}}" rel="stylesheet">--}}
+    {{--<link href="{{baseUrlConcat('/classimax/plugins/seiyria-bootstrap-slider/dist/css/bootstrap-slider.min.css')}}" rel="stylesheet">--}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lity/2.3.0/lity.min.css">
     <!-- CUSTOM CSS -->
-    <link href="{{asset('classimax/css/style.css')}}" rel="stylesheet">
-    {{--<link href="{{ asset('css/app.css') }}" rel="stylesheet">--}}
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link href="{{baseUrlConcat('/classimax/css/style.css')}}" rel="stylesheet">
+    {{--<link href="{{ baseUrlConcat('/css/app.css') }}" rel="stylesheet">--}}
+    <link href="{{ baseUrlConcat('/css/style.css') }}" rel="stylesheet">
 
 
     <!-- FAVICON -->
@@ -144,21 +144,21 @@
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-{{--        <script src="{{asset('classimax/plugins/jquery/jquery.js')}}"></script>--}}
-        <script src="{{asset('classimax/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
-{{--        <script src="{{asset('classimax/plugins/tether/js/tether.min.js')}}"></script>--}}
-        <script src="{{asset('classimax/plugins/raty/jquery.raty-fa.js')}}"></script>
-        <script src="{{asset('classimax/plugins/bootstrap/dist/js/popper.min.js')}}"></script>
-        <script src="{{asset('classimax/plugins/bootstrap/dist/js/bootstrap.min.js')}}"></script>
-        <script src="{{asset('classimax/plugins/seiyria-bootstrap-slider/dist/bootstrap-slider.min.js')}}"></script>
-        <script src="{{asset('classimax/plugins/slick-carousel/slick/slick.min.js')}}"></script>
-        <script src="{{asset('classimax/plugins/jquery-nice-select/js/jquery.nice-select.min.js')}}"></script>
-        <script src="{{asset('classimax/plugins/fancybox/jquery.fancybox.pack.js')}}"></script>
-        <script src="{{asset('classimax/plugins/smoothscroll/SmoothScroll.min.js')}}"></script>
-        <script src="{{asset('classimax/js/scripts.js')}}"></script>
+{{--        <script src="{{baseUrlConcat('/classimax/plugins/jquery/jquery.js')}}"></script>--}}
+        <script src="{{baseUrlConcat('/---classimax/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
+{{--        <script src="{{baseUrlConcat('/classimax/plugins/tether/js/tether.min.js')}}"></script>--}}
+        <script src="{{baseUrlConcat('/classimax/plugins/raty/jquery.raty-fa.js')}}"></script>
+        <script src="{{baseUrlConcat('/classimax/plugins/bootstrap/dist/js/popper.min.js')}}"></script>
+        <script src="{{baseUrlConcat('/classimax/plugins/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+        <script src="{{baseUrlConcat('/classimax/plugins/seiyria-bootstrap-slider/dist/bootstrap-slider.min.js')}}"></script>
+        <script src="{{baseUrlConcat('/classimax/plugins/slick-carousel/slick/slick.min.js')}}"></script>
+        <script src="{{baseUrlConcat('/classimax/plugins/jquery-nice-select/js/jquery.nice-select.min.js')}}"></script>
+        <script src="{{baseUrlConcat('/classimax/plugins/fancybox/jquery.fancybox.pack.js')}}"></script>
+        <script src="{{baseUrlConcat('/classimax/plugins/smoothscroll/SmoothScroll.min.js')}}"></script>
+        <script src="{{baseUrlConcat('/classimax/js/scripts.js')}}"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/lity/2.3.0/lity.min.js"></script>
-<script src="{{ asset('js/stripe.js') }}"></script>
-<script src="{{ asset('js/index.js') }}"></script>
+<script src="{{ baseUrlConcat('/js/stripe.js') }}"></script>
+<script src="{{ baseUrlConcat('/js/index.js') }}"></script>
 @yield('footer')
 
 </body>
