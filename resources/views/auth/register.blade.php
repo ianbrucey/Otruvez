49261601d053d9fbc6a906c7122aa0906f4362e1
ianbrucey <ib708090@gmail.com>
2018-09-19 04:17:16 -0700

    reducing social auth buttons

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,14 +14,7 @@
                     <h3 class="card-heading text-center"><img src="{{getOtruvezLogoImg()}}" width="100"></h3>
                     <h3 class="card-heading text-center">Register</h3>
                 </div>
-                <div class="text-center auth-login-button-container">
-                    <p>Register with</p>
-                    <a href="{{ url('/auth/facebook') }}" class="btn text-white" style="background: #4267b2;"><i class="fa fa-facebook"></i> Facebook</a>
-                    <a href="{{ url('/auth/google') }}" class="btn btn-danger"><i class="fa fa-google"></i> Google</a>
-                    <a href="{{ url('/auth/twitter') }}" class="btn text-white" style="background: lightskyblue;"><i class="fa fa-google"></i> Twitter</a>
-                    <hr>
-                </div>
-
+                @include('partials.social.auth-buttons')
 
                 <div class="card-body">
                     <form class="form-horizontal validate-register" method="POST" action="{{ route('register') }}">
--- resources/views/partials/social/auth-buttons.blade.php
+++ resources/views/partials/social/auth-buttons.blade.php
@@ -1,7 +1,7 @@
 <div class="text-center auth-login-button-container">
     <p class="text-center">Sign in with</p>
     <a href="{{ url('/auth/facebook') }}" class="btn text-white" style="background: #4267b2;"><i class="fa fa-facebook"></i> Facebook</a>
-    <a href="{{ url('/auth/google') }}" class="btn btn-danger"><i class="fa fa-google"></i> Google</a>
-    <a href="{{ url('/auth/twitter') }}" class="btn text-white" style="background: lightskyblue;"><i class="fa fa-google"></i> Twitter</a>
+    {{--<a href="{{ url('/auth/google') }}" class="btn btn-danger"><i class="fa fa-google"></i> Google</a>--}}
+    {{--<a href="{{ url('/auth/twitter') }}" class="btn text-white" style="background: lightskyblue;"><i class="fa fa-google"></i> Twitter</a>--}}
     <hr>
 </div>
