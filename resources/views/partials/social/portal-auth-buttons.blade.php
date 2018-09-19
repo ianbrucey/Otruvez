b103cb72681a96292d15429e9b9015459b47a52b
ianbrucey <ib708090@gmail.com>
2018-09-19 04:06:01 -0700

    Revert "adding composer files and things"
    This reverts commit 87e1b98

--- resources/views/partials/social/portal-auth-buttons.blade.php
+++ resources/views/partials/social/portal-auth-buttons.blade.php
@@ -1,7 +1,7 @@
 <div class="text-center auth-login-button-container">
     <p class="text-center">Sign in with</p>
     <a href="{{ url('/portal/auth/facebook') }}{{$portalRouteExtension}}" class="btn text-white" style="background: #4267b2;"><i class="fa fa-facebook"></i> Facebook</a>
-    {{--<a href="{{ url('/portal/auth/google') }}{{$portalRouteExtension}}" class="btn btn-danger"><i class="fa fa-google"></i> Google</a>--}}
-    {{--<a href="{{ url('/portal/auth/twitter') }}{{$portalRouteExtension}}" class="btn text-white" style="background: lightskyblue;"><i class="fa fa-google"></i> Twitter</a>--}}
+    <a href="{{ url('/portal/auth/google') }}{{$portalRouteExtension}}" class="btn btn-danger"><i class="fa fa-google"></i> Google</a>
+    <a href="{{ url('/portal/auth/twitter') }}{{$portalRouteExtension}}" class="btn text-white" style="background: lightskyblue;"><i class="fa fa-google"></i> Twitter</a>
     <hr>
 </div>
