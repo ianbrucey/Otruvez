b6529d7259935ddd5c66d21308593e36a4aaa3cc
root <root@ip-172-31-18-52.us-west-2.compute.internal>
2019-03-10 22:58:39 +0000

    committing vendor files

---
+++ vendor/vendor_stable/laravel/framework/src/Illuminate/Foundation/Auth/RegistersUsers.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Illuminate\Foundation\Auth;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Auth\Events\Registered;
+
+trait RegistersUsers
+{
+    use RedirectsUsers;
+
+    /**
+     * Show the application registration form.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function showRegistrationForm()
+    {
+        return view('auth.register');
+    }
+
+    /**
+     * Handle a registration request for the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        $this->validator($request->all())->validate();
+
+        event(new Registered($user = $this->create($request->all(), $request)));
+
+        $this->guard()->login($user);
+
+        return $this->registered($request, $user)
+                        ?: redirect($this->redirectPath());
+    }
+
+    /**
+     * Get the guard to be used during registration.
+     *
+     * @return \Illuminate\Contracts\Auth\StatefulGuard
+     */
+    protected function guard()
+    {
+        return Auth::guard();
+    }
+
+    /**
+     * The user has been registered.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $user
+     * @return mixed
+     */
+    protected function registered(Request $request, $user)
+    {
+
+        if($user->activated != "1" && $request->has('apiKey')) {
+            return redirect($this->confirmAccountRoute);
+        } elseif($user->activated != "1") {
+            return redirect('/confirmAccount');
+        } elseif($request->has('apiKey')) {
+            return redirect($this->viewServiceRoute);
+        }
+
+        if($request->has('business_owner') && $request->get('business_owner') == true) {
+            return redirect('/business');
+        }
+
+        return redirect('/home')->with("successMessage", "Registration successful! Now get subscribing");
+    }
+}
